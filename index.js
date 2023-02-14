bccd0a14fbfbef06cbb4e81f5550567e02a3fdca
Bret Comnes <bcomnes@gmail.com>
2023-02-14 16:07:25 -0700

    Add depDiffCommentsEnabled setting
    This settings lets one disable dependency diff comments in the bot. Who would want to do that!?

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const { socketYmlSchemaV1 } = require('./lib/v1')
  * @property {string[]} [ignoreUsers] list of GitHub usernames to ignore when creating reports
  * @property {boolean} [projectReportsEnabled] enable/disable Github app project report checks
  * @property {boolean} [pullRequestAlertsEnabled] enable/disable GitHub app pull request alert checks
+ * @property {boolean} [depDiffCommentsEnabled] enable/disable Pull request comments with details about changed dependencies
  */
 
 /**
@@ -52,6 +53,7 @@ const socketYmlSchema = {
         },
         projectReportsEnabled: { type: 'boolean', nullable: true },
         pullRequestAlertsEnabled: { type: 'boolean', nullable: true },
+        depDiffCommentsEnabled: { type: 'boolean', nullable: true }
       },
       required: [],
       additionalProperties: false,
